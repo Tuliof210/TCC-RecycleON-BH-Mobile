420c239541422c897d94fefbd72015a796a74003
Tulio Ferreira <tuliof210@gmail.com>
2021-09-08 12:18:41 -0300

    feat: setup colors

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { colors } from './constants/styles';
+import { colors } from './src/constants/styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,8 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.get('green-light'),
+    backgroundColor: colors.get('green-dark'),
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: colors.get('white'),
   },
 });
--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,1 +1,6 @@
-export const colors = new Map<string, string>([['green-light', '#87D459']]);
+export const colors = new Map<string, string>([
+  ['black-light', '#263238'],
+  ['green-dark', '#489C17'],
+  ['green-light', '#87D459'],
+  ['white', '#fff'],
+]);
