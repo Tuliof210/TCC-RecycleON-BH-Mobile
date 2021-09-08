7f82f26e92bc99b1f8d7811fb912579f9877ba91
Tulio Ferreira <tuliof210@gmail.com>
2021-09-08 12:03:34 -0300

    feat: set default color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { colors } from './constants/styles';
 
 export default function App() {
   return (
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.get('green-light'),
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/constants/styles.ts
@@ -0,0 +1,1 @@
+export const colors = new Map<string, string>([['green-light', '#87D459']]);
