b731e9014004e22c10ec6fd50795d0f574903e02
Tulio Ferreira <tuliof210@gmail.com>
2021-10-19 09:49:00 -0300

    feat: prototype profile screen

--- src/screens/app/profile/profile-card/profile-card.component.tsx
+++ src/screens/app/profile/profile-card/profile-card.component.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { Text, TouchableHighlight, View } from 'react-native';
 
 import styles from './profile-card.style';
+import { highLightColors } from 'common/constants/colors';
 
 export function ProfileCardComponent(props: { name: string | undefined; email: string | undefined }): JSX.Element {
+  const test = () => {
+    alert('Pressed!');
+  };
   return (
-    <View style={styles.container}>
-      <Text style={[styles.label]}>{props.name}</Text>
-      <View style={styles.breakLine} />
-      <Text style={[styles.label]}>{props.email}</Text>
-    </View>
+    <Fragment>
+      <View style={[styles.container, styles.containerShadow]}>
+        <Text style={[styles.label]}>{props.name}</Text>
+        <View style={styles.breakLine} />
+        <Text style={[styles.label]}>{props.email}</Text>
+      </View>
+
+      <View style={[styles.container, styles.containerShadow, styles.containerOptions]}>
+        <TouchableHighlight activeOpacity={1} underlayColor={highLightColors('green-light')} onPress={test}>
+          <Text style={styles.menuOption}>Alterar Dados</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight activeOpacity={1} underlayColor={highLightColors('green-light')} onPress={test}>
+          <Text style={styles.menuOption}>Locais Favoritos</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          activeOpacity={1}
+          underlayColor={highLightColors('red')}
+          onPress={test}
+          style={{ zIndex: -11 }}
+        >
+          <Text style={[styles.menuOption, styles.exit]}>Sair</Text>
+        </TouchableHighlight>
+      </View>
+    </Fragment>
   );
 }
--- src/screens/app/profile/profile-card/profile-card.style.ts
+++ src/screens/app/profile/profile-card/profile-card.style.ts
@@ -3,19 +3,37 @@ import { colors } from 'common/constants/colors';
 
 export default StyleSheet.create({
   container: {
-    marginHorizontal: 25,
-    padding: 15,
-    backgroundColor: colors('gray-border'),
-    borderRadius: 10,
+    marginVertical: 5,
+    marginHorizontal: 10,
+    // padding: 15,
+    backgroundColor: colors('white-dark'),
+    borderRadius: 7,
   },
+  containerShadow: {
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  containerOptions: {},
   label: {
     textAlign: 'center',
-    fontSize: 15,
+    fontSize: 25,
     fontFamily: 'Ubuntu-Regular',
   },
   breakLine: {
     height: 1,
     marginVertical: 5,
     backgroundColor: colors('gray-line'),
+  },
+  menuOption: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    fontSize: 15,
+    fontFamily: 'Ubuntu-Medium',
+    color: colors('gray-font-dark'),
+  },
+  exit: {
+    color: colors('red'),
   },
 });
